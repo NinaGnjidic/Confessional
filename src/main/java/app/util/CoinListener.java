97e8fd49f6c724224a2c40688e14ebba11159e4c
andrija <andrija.gro@gmail.com>
2025-09-06 22:27:27 +0200

    Add CoinListener

--- src/main/java/app/util/CoinListener.java
+++ src/main/java/app/util/CoinListener.java
@@ -1,18 +1,29 @@
 package main.java.app.util;
 
-import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortEvent;
+import com.fazecast.jSerialComm.SerialPortMessageListener;
 
-public class CoinListener implements SerialPortDataListener {
+public class CoinListener implements SerialPortMessageListener {
 	@Override
 	public int getListeningEvents() {
-		// TODO Auto-generated method stub
-		return 0;
+		return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+	}
+
+	@Override
+	public byte[] getMessageDelimiter() {
+		return new byte[] { (byte) '\r', (byte) '\n' };
+	}
+
+	@Override
+	public boolean delimiterIndicatesEndOfMessage() {
+		return true;
 	}
 
 	@Override
 	public void serialEvent(SerialPortEvent event) {
-		// TODO Auto-generated method stub
-		
+		byte[] data = event.getReceivedData();
+		String message = new String(data).strip();
+		System.out.println(message);
 	}
 }
